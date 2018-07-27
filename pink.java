d61bf2789f19cbd3a0405ad50318baa6e3c118da
Sravyareddy23 <41725851+Sravyareddy23@users.noreply.github.com>
2018-07-27 14:21:05 +0530

    Update pink.java

--- pink.java
+++ pink.java
@@ -1,1 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner.*;
 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for (i = 1; i <=3; i++) {
+			System.out.println("Hello\n");
+		}// your code goes here
+	}
+}
